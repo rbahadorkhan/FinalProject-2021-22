db3d2f77dc039fcf7666ffd22e4abc2734487969
KaiyanChu <kchu@bayviewglen.ca>
2021-12-15 14:22:26 -0500

    charachter methods

--- TextAdventure/src/zork/Characters.java
+++ TextAdventure/src/zork/Characters.java
@@ -7,6 +7,29 @@ public class Characters {
     private String startingRoom;
     private int hp;
     private int attack;
+
+
+    public Characters(String id, String name, String description, String startingRoom, int hp, int attack){
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.startingRoom = startingRoom;
+        this.hp = hp;
+        this.attack = attack;
+    }
+
+    public int getHp(){
+        return hp;
+    }
     
+    public int getAttack(){
+        return attack; 
+    }
+
+    public void changeHp(Character cha, int reduced){
+        hp -= reduced; 
+    }
     
+
+
 }
