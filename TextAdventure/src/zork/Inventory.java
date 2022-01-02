b5edb832983d0f5831e3cc6e2febafc3cc982a24
Yola_Xu <zxu@bayviewglen.ca>
2022-01-02 13:47:06 -0500

    inventory

--- TextAdventure/src/zork/Inventory.java
+++ TextAdventure/src/zork/Inventory.java
@@ -2,14 +2,10 @@ package zork;
 
 import java.util.ArrayList;
 
-
 public class Inventory {
-  
   private ArrayList<Item> items;
   private int maxWeight;
   private int currentWeight;
-  
-    
 
   public Inventory(int maxWeight) {
     this.items = new ArrayList<Item>();
@@ -24,24 +20,7 @@ public class Inventory {
   public int getCurrentWeight() {
     return currentWeight;
   }
-  
-  public boolean addItem (Command com, Item item) {
-    if (!com.hasSecondWord()){
-      System.out.println("What do you want to take?");
 
-    } else if (com.getSecondWord().toString() != item.getName()){
-      System.out.println("This item doesn't exist");
-
-    } else if (com.getSecondWord().toString().equals(item.getName())) {
-      return items.add(item);
-
-    } else {
-      System.out.println("There is no space in your inventory for this item, drop it or something else.");
-    }
-    return true; 
-    //work on it after commands
-
-  }
 
   public boolean addItem(Item item) {
     if (item.getWeight() + currentWeight <= maxWeight)
@@ -51,14 +30,6 @@ public class Inventory {
       return false;
     }
   }
-
-  public boolean dropItem (Command com){
-    if (com.getSecondWord().size() > 0) {
-      for (int i = items.size(); i >= 0; i--) {
-        //finish later
-      }
-    }
-    return true; //just until i finish it
-  }
-
 }
+
+
