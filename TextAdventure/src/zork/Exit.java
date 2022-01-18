bf89f3f93e92b17881eaca1941fa1d1404b95611
Romina Bahadorkhan <rbahadorkhan@bayviewglen.ca>
2022-01-18 11:44:37 -0500

    exit

--- TextAdventure/src/zork/Exit.java
+++ TextAdventure/src/zork/Exit.java
@@ -18,24 +18,28 @@ public class Exit extends OpenableObject {
     this.direction = direction;
     this.adjacentRoom = adjacentRoom;
   }
-
+// 
   public Exit(String direction, String adjacentRoom) {
     this.direction = direction;
     this.adjacentRoom = adjacentRoom;
   }
 
+  //gives and shows the directions of the exits
   public String getDirection() {
     return direction;
   }
 
+  //sets the direction of the exits
   public void setDirection(String direction) {
     this.direction = direction;
   }
 
+  //gives and shows us the room next to the room you are in
   public String getAdjacentRoom() {
     return adjacentRoom;
   }
 
+ //sets a room next to the room you are in
   public void setAdjacentRoom(String adjacentRoom) {
     this.adjacentRoom = adjacentRoom;
   }
