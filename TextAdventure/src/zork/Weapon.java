781babecb015cbf22c9422bfda444086b92d21d6
KaiyanChu <kchu@bayviewglen.ca>
2022-01-18 00:16:45 -0500

    rooms json fixed, comments on weapon

--- TextAdventure/src/zork/Weapon.java
+++ TextAdventure/src/zork/Weapon.java
@@ -5,6 +5,7 @@ public class Weapon extends Item {
     private int accuracy;
     private int numBullets;
 
+    //weapon constructor 
     public Weapon(int itemWeight, int damage, int accuracy, int numBullets, String name, String startingRoom, boolean isWeapon, boolean isHealing, boolean isSpike, String desciption) { 
         super(itemWeight, name, startingRoom, isWeapon, isHealing, isSpike, desciption);
         this.damage = damage;
@@ -12,18 +13,22 @@ public class Weapon extends Item {
         this.numBullets = numBullets;
       }
 
+      //returns the number of bullets
       private int getNumBullets() {
         return numBullets;
       }
     
+      //returns the damage per bullet
       private int getDamage() {
         return damage;
       }
     
+      //returns the accuracy of boots
       private int getAccuracy(){
         return accuracy;
       }
     
+      //calculates the totaldamage
       public int damageDealt(){
         int totalDamage = 0;
         for (int i = 0; i < this.getNumBullets(); i++) {
